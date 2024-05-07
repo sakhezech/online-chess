0ba19d9739db49baa7967d549a46d57b7c1d8f40
sakhezech <contact@sakhezech.ru>
2024-05-07 21:50:49 +0300

    refactor(chess): better move attr names

--- src/chess/move.py
+++ src/chess/move.py
@@ -4,21 +4,21 @@ from .util import index_to_square, square_to_index
 
 
 class Move(NamedTuple):
-    from_idx: int
-    to_idx: int
+    origin: int
+    dest: int
     promotion: str = ''
 
     def __repr__(self) -> str:
-        from_square = index_to_square(self.from_idx)
-        to_square = index_to_square(self.to_idx)
-        sttr = f'{from_square}{to_square}{self.promotion}'
-        return f"{self.__class__.__name__}('{sttr}')"
+        from_square = index_to_square(self.origin)
+        to_square = index_to_square(self.dest)
+        uci = f'{from_square}{to_square}{self.promotion}'
+        return f"{self.__class__.__name__}('{uci}')"
 
     @classmethod
     def from_uci(cls, uci: str):
         if not 4 <= len(uci) <= 5:
             raise ValueError
-        from_idx = square_to_index(uci[0:2])
-        to_idx = square_to_index(uci[2:4])
+        origin = square_to_index(uci[0:2])
+        dest = square_to_index(uci[2:4])
         promotion = uci[4]
-        return cls(from_idx, to_idx, promotion)
+        return cls(origin, dest, promotion)
