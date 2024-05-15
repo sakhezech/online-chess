7a3b5a9c972d199eeddb7141ea89a508034ee31f
sakhezech <contact@sakhezech.ru>
2024-05-15 21:29:33 +0300

    feat(chess): CastleRights with_* convenience functions

--- src/chess/castlerights.py
+++ src/chess/castlerights.py
@@ -13,3 +13,9 @@ class CastleRights(NamedTuple):
     @classmethod
     def from_bools(cls, K: bool, Q: bool, k: bool, q: bool):
         return cls(SideRights(K, Q), SideRights(k, q))
+
+    def with_white(self, kingside: bool, queenside: bool):
+        return self.__class__(SideRights(kingside, queenside), self.black)
+
+    def with_black(self, kingside: bool, queenside: bool):
+        return self.__class__(self.white, SideRights(kingside, queenside))
