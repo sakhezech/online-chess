bb03310334a2db2000d85b2930ed238002f10aee
sakhezech <contact@sakhezech.ru>
2024-05-24 23:46:58 +0300

    test(chess): more perft tests

--- tests/test_perft.py
+++ tests/test_perft.py
@@ -1,5 +1,6 @@
 import copy
 
+import pytest
 from chess.board import Board
 from chess.pieces import Piece
 
@@ -22,19 +23,47 @@ def get_number_of_moves(board: Board, depth: int):
     return sum
 
 
-perft_results = {
-    1: 20,
-    2: 400,
-    3: 8_902,
-    4: 197_281,
-    5: 4_865_609,
-    6: 119_060_324,
-    7: 3_195_901_860,
-}
+# https://www.chessprogramming.org/Perft_Results
 
 
-def test_perft(perft_depth: int):
+@pytest.mark.perft
+def test_perft():
     board = Board()
-    assert (
-        get_number_of_moves(board, perft_depth) == perft_results[perft_depth]
+    assert get_number_of_moves(board, 4) == 197281
+
+
+@pytest.mark.perft
+def test_another_perft2():
+    board = Board(
+        'r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1'
     )
+    assert get_number_of_moves(board, 3) == 97862
+
+
+@pytest.mark.perft
+def test_another_perft3():
+    board = Board('8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1')
+    assert get_number_of_moves(board, 4) == 43238
+
+
+@pytest.mark.perft
+def test_another_perft4():
+    board = Board(
+        'r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1'
+    )
+    assert get_number_of_moves(board, 3) == 9467
+
+
+@pytest.mark.perft
+def test_another_perft5():
+    board = Board('rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8')
+    assert get_number_of_moves(board, 3) == 62379
+
+
+@pytest.mark.perft
+def test_another_perft6():
+    board = Board(
+        'r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1'
+        ' w - - 0 10'
+    )
+    assert get_number_of_moves(board, 3) == 89890
