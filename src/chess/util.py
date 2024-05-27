4f0dad111b471cb7bc783fc64b131f74a59ec2a9
sakhezech <contact@sakhezech.ru>
2024-05-27 18:24:08 +0300

    feat(chess): square to index and index to square validation

--- src/chess/util.py
+++ src/chess/util.py
@@ -4,11 +4,19 @@ from typing import NamedTuple
 
 
 def square_to_index(square: str) -> int:
-    col, row = square[0].lower(), square[1]
-    return 20 + (ord(col) - 96) + (8 - int(row)) * 10
+    if len(square) != 2:
+        raise ValueError(f'not a square: {square}')
+    col, row = ord(square[0].lower()), int(square[1])
+    if not 1 <= row <= 8:
+        raise ValueError('row is not between 1 and 8: {square[1]}')
+    if not 97 <= col <= 105:
+        raise ValueError(f'column is not between a and h: {square[0].lower()}')
+    return 20 + (col - 96) + (8 - row) * 10
 
 
 def index_to_square(index: int) -> str:
+    if not 21 <= index <= 98 or index % 10 in {0, 9}:
+        raise ValueError(f'index not on board: {index}')
     row_something, col_idx = divmod(index - 20, 10)
     letter = chr(96 + col_idx)
     return f'{letter}{8 - row_something}'
