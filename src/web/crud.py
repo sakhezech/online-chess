0c7951e57b90abdc5366e20bb67aa1b094e70c05
sakhezech <contact@sakhezech.ru>
2024-07-18 02:29:15 +0300

    feat(web): add chess related crud funcs

--- src/web/crud.py
+++ src/web/crud.py
@@ -37,3 +37,24 @@ def delete_session_by_session_id(db: Session, session_id: str) -> None:
     stmt = delete(m.Session).where(m.Session.session_id == session_id)
     db.execute(stmt)
     db.commit()
+
+
+def create_chess(db: Session, fen: str | None = None) -> m.Chess:
+    chess = m.Chess(fen=fen)
+    db.add(chess)
+    db.commit()
+    db.refresh(chess)
+    return chess
+
+
+def get_chess_by_chess_id(db: Session, chess_id: str) -> m.Chess | None:
+    stmt = select(m.Chess).where(m.Chess.chess_id == chess_id)
+    return db.execute(stmt).scalar()
+
+
+def create_move(db: Session, chess_id: str, user_id: str, fen: str) -> m.Move:
+    move = m.Move(chess_id, user_id, fen)
+    db.add(move)
+    db.commit()
+    db.refresh(move)
+    return move
